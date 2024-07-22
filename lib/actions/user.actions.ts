a32ad4adb4c2d142243a84d76b301511b3bb340c
kozdailyy <kazockparfait02@gmail.com>
2024-07-22 23:07:34 +0200

    Implement Frontend of Auth Pages

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+    // Mutations / Database / Fetch
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    // Create User Account
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,7 @@
 import { type ClassValue, clsx } from "clsx";
 import qs from "query-string";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -203,3 +204,25 @@ export const getTransactionStatus = (date: Date) => {
 
   return date > twoDaysAgo ? "Processing" : "Success";
 };
+
+export const authFormSchema = (type: string) =>
+  z.object({
+    // sign up
+    firstName:
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(50),
+    lastName:
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(50),
+    address1:
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(30),
+    city:
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(30),
+    state:
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(20),
+    postalCode: type === "sign-in" ? z.string().optional() : z.string().min(4),
+    dateOfBirth:
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(30),
+    ssn: type === "sign-in" ? z.string().optional() : z.string().min(3).max(30),
+    // both
+    email: z.string().email(),
+    password: z.string().min(8).max(20),
+  });
